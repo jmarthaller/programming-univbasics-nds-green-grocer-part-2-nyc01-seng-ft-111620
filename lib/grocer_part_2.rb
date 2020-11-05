9d898287d6cb90ff68c31d9986cec5f94b271d49
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-05 17:39:02 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,103 +1,142 @@
 require_relative './part_1_solution.rb'
 require 'pry'
 
-def apply_coupons(cart, coupons)
-  if coupons.length == 0
-    cart
-  else
-    result = []
-    cart.each do |grocery_item|
-      coupons.each do |coupon|
+# def apply_coupons(cart, coupons)
+#   if coupons.length == 0
+#     cart
+#   else
+#     result = []
+#     cart.each do |grocery_item|
+#       coupons.each do |coupon|
       
-        if grocery_item[:item] == coupon[:item]
-          coupon[:item] = coupon[:item] + " W/COUPON"
+#         if grocery_item[:item] == coupon[:item]
+#           coupon[:item] = coupon[:item] + " W/COUPON"
           
-          if grocery_item[:clearance] == true
-            coupon[:clearance] = true
-          else 
-            coupon[:clearance] = false
-          end
+#           if grocery_item[:clearance] == true
+#             coupon[:clearance] = true
+#           else 
+#             coupon[:clearance] = false
+#           end
           
-          coupon[:price] = coupon[:cost] / coupon[:num]
-          coupon[:count] = coupon[:num]
-          coupon.delete(:cost); coupon.delete(:num)
-          grocery_item[:count] = grocery_item[:count] - coupon[:count]
-          result << coupon
-        end
-      end
-      result << grocery_item
-    end
-    result
-  end
-end
+#           coupon[:price] = coupon[:cost] / coupon[:num]
+#           coupon[:count] = coupon[:num]
+#           coupon.delete(:cost); coupon.delete(:num)
+#           grocery_item[:count] = grocery_item[:count] - coupon[:count]
+#           result << coupon
+#         end
+#       end
+#       result << grocery_item
+#     end
+#     result
+#   end
+# end
+
+
+# # def apply_clearance(cart)
+# #   update_array = []
+# #   cart.each do |grocery_item|
+# #     if grocery_item[:clearance] == true
+# #       grocery_item[:price] = (grocery_item[:price] * 0.8).round(2)
+# #     end
+# #     update_array << grocery_item
+# #   end
+# #   update_array
+# # end
+
+
+# # def checkout(cart, coupons)
+# # consolidated = consolidate_cart(cart)
+# # post_coupon = apply_coupons(consolidated, coupons)
+# # post_clearance = apply_clearance(post_coupon)
+# # grand_total = 0
+ 
+# # post_clearance.each do |grocery_item|
+# #   grand_total += (grocery_item[:price] * grocery_item[:count])
+# # end
+  
+# # if grand_total > 100
+# #   grand_total *= 0.90
+# # else 
+# #   grand_total
+# # end
+# # end
+
+
+
 
 
 # def apply_clearance(cart)
-#   update_array = []
-#   cart.each do |grocery_item|
-#     if grocery_item[:clearance] == true
-#       grocery_item[:price] = (grocery_item[:price] * 0.8).round(2)
+#   cart.each do |entry|
+#     if entry[:clearance] == true
+#       entry[:price] = (entry[:price] * 0.80).round(2)
 #     end
-#     update_array << grocery_item
 #   end
-#   update_array
 # end
+
+
 
 
 # def checkout(cart, coupons)
-# consolidated = consolidate_cart(cart)
-# post_coupon = apply_coupons(consolidated, coupons)
-# post_clearance = apply_clearance(post_coupon)
-# grand_total = 0
- 
-# post_clearance.each do |grocery_item|
-#   grand_total += (grocery_item[:price] * grocery_item[:count])
-# end
-  
-# if grand_total > 100
-#   grand_total *= 0.90
-# else 
+#   consolidated = consolidate_cart(cart)
+#   couponed = apply_coupons(consolidated, coupons)
+#   clearanced = apply_clearance(couponed)
+#   grand_total = 0
+#   clearanced.each do |entry|
+#     grand_total += entry[:price] * entry[:count]
+#   end
+#   if grand_total > 100 
+#     grand_total *= 0.90
+#   end
 #   grand_total
 # end
-# end
-
-
 
 
 
 def apply_clearance(cart)
   cart.each do |entry|
-    if entry[:clearance] == true
-      entry[:price] = (entry[:price] * 0.80).round(2)
+    entry[:price] = (entry[:price] * 0.80).round(2)  if entry[:clearance] == true
+  end
+end
+
+
+def apply_coupons(cart, coupons)
+  result = []
+  if coupons.length == 0
+    cart
+  else
+    cart.each do |entry|
+      coupons.each do |coupon|
+        if entry[:item] == coupon[:item]
+          coupon[:item] += " W/COUPON"
+          coupon[:count] = coupon[:num]
+          coupon[:clearance] = entry[:clearance]
+          coupon[:price] = coupon[:cost] / coupon[:count]
+          coupon.delete(:cost)
+          coupon.delete(:num)
+          entry[:count] -= coupon[:count]
+          result << coupon
+        end
+      end
+      result << entry
     end
+     result
   end
 end
 
 
 
 
-def checkout(cart, coupons)
-  consolidated = consolidate_cart(cart)
+def checkout(grocery_item_cart, coupons)
+  consolidated = consolidate_cart(grocery_item_cart)
   couponed = apply_coupons(consolidated, coupons)
   clearanced = apply_clearance(couponed)
   grand_total = 0
   clearanced.each do |entry|
     grand_total += entry[:price] * entry[:count]
   end
-  if grand_total > 100 
-    grand_total *= 0.90
-  end
+  return grand_total * 0.90 if grand_total > 100
   grand_total
 end
-
-
-
-
-
-
-
-
-
 
 
 
--- lib/part_1_solution.rb
+++ lib/part_1_solution.rb
@@ -13,7 +13,6 @@ end
 def consolidate_cart(cart)
   index = 0
   new_cart = []
-  
   cart.each do |grocery_item|
     current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
     if current_item
